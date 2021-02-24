8702c9da25db8cfdc7d8a7974a94a6b4d2d75274
Tim <timothe.didierjean2@gmail.com>
2021-02-25 00:24:04 +0100

    add custom stript in utils.js

--- app/javascript/widget/helpers/utils.js
+++ app/javascript/widget/helpers/utils.js
@@ -26,3 +26,21 @@ export const IFrameHelper = {
   },
   getMessage: e => JSON.parse(e.data.replace(WOOT_PREFIX, '')),
 };
+
+window.addEventListener('chatwoot:ready', function () {	  
+  window.$chatwoot.onclick = function(event) { 
+		alert("click");
+		if (event.target.localName == 'a') {
+			alert('a_target');
+			if (event.target.href.split('.')[0] == 'https://postback') {
+				alert('postback target');
+				payload = event.target.href.split('.')[1].replace('/','').replace('\\','')
+				sender_id = event.target.href.split('.')[2].replace('/','').replace('\\','')
+				var xmlHttp = new XMLHttpRequest();
+				xmlHttp.open( "GET", "https://escapade-6t98.herokuapp.com/postback?payload=" + payload + "&sender_id=" + sender_id, true ); // false for synchronous request
+				xmlHttp.send( null );
+				event.preventDefault();
+			}
+		}
+	};
+});
